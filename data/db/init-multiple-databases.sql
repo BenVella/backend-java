1a86cc19bde3ec62815133cfc3dc51f1d9fcd497
Bennet Vella <bennetvella@gmail.com>
2025-04-19 17:27:01 +0200

    Should be the final configuration for postgres with two separate dbs that are available for keycloak and the app independently, on two separate users

--- data/db/init-multiple-databases.sql
+++ data/db/init-multiple-databases.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE keycloak_db;
 CREATE DATABASE app_db;
 
-GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO admin;
-GRANT ALL PRIVILEGES ON DATABASE app_db TO admin;
-
+-- Keycloak DB Service User
 -- Create a new user for Keycloak
-CREATE USER keycloak_service WITH PASSWORD 'keycloak_password';
+CREATE USER keycloak_service WITH ENCRYPTED PASSWORD 'keycloak_password';
 
--- Grant the necessary permissions to the keycloak_service user on keycloak_db
-GRANT CONNECT ON DATABASE keycloak_db TO keycloak_service;
-GRANT USAGE ON SCHEMA public TO keycloak_service;
-GRANT CREATE, SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO keycloak_service;
-GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO keycloak_service;
+GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak_service;
+-- See https://stackoverflow.com/a/75876944/2179720 for details on why we connect first
+\c keycloak_db postgres
+GRANT ALL ON SCHEMA public TO keycloak_service;
 
--- Ensure future tables and sequences also have the correct permissions
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO keycloak_service;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO keycloak_service;
+-- APP DB Service User
+-- Create a new user for the application
+CREATE USER app_service WITH ENCRYPTED PASSWORD 'app_password';
+
+-- Grant the necessary permissions to the app_user on app_db
+GRANT ALL PRIVILEGES ON DATABASE app_db TO app_service;
+\c app_db postgres
+GRANT ALL ON SCHEMA public TO app_service;
